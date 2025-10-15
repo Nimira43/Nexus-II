8a2ab95f8ab11917232dd62299834cd61c9edf73
Nimira43 <n.rabone72@aol.com>
2025-10-15 18:09:33 +0100

    Create getUsers method

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,8 @@
 import { Controller } from '@nestjs/common'
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  public getUsers() {
+    return 'You have sent a GET request to users endpoint'
+  }
+}
