a46350bdb4bbcc981ed5d0264aaebca9b5c6b607
Nimira43 <n.rabone72@aol.com>
2025-10-15 18:10:34 +0100

    Added Get decorator to getUsers method

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get } from '@nestjs/common'
 
 @Controller('users')
 export class UsersController {
+  @Get()
   public getUsers() {
     return 'You have sent a GET request to users endpoint'
   }
