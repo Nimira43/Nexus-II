d6ba581c2d52899cce9c8189c0442c5d3d01767c
Nimira43 <n.rabone72@aol.com>
2025-12-15 21:01:37 +0000

    Cleaned up new PatchUserDto

--- src/users/dtos/patch-user.dto.ts
+++ src/users/dtos/patch-user.dto.ts
@@ -1,27 +1,3 @@
 import { IsString, IsNotEmpty, MinLength, MaxLength, IsOptional, IsEmail, Matches } from 'class-validator'
 
-export class CreateUserDto {  
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(3)
-  @MaxLength(96)
-  firstName: string
-  
-  @IsString()
-  @IsOptional()
-  @MinLength(3)
-  @MaxLength(96)
-  lastName?: string
-
-  @IsEmail()
-  @IsNotEmpty()
-  email: string
-
-  @IsString()
-  @IsNotEmpty()
-  @MinLength(8)
-  @Matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, {
-    message: 'Password needs to have minimum of eight characters, at least one letter, one number and one special character.'
-  })
-  password: string
-}
+export class PatchUserDto {  }
