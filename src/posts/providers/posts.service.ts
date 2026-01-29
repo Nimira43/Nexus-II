db828bb4b8ed814440f1606ec16e2cb68a53781e
Nimira43 <n.rabone72@aol.com>
2026-01-29 22:33:47 +0000

    Return posts from PostService

--- src/posts/providers/posts.service.ts
+++ src/posts/providers/posts.service.ts
@@ -4,5 +4,16 @@ import { Injectable } from '@nestjs/common'
 export class PostsService {
   public findAll(userId: string) {
     console.log(userId)
+
+    return [
+      {
+        title: 'Test Title 1',
+        content: 'Test Content 1'
+      },
+      {
+        title: 'Test Title 2',
+        content: 'Test Content 2'
+      }
+    ]
   }
 }
