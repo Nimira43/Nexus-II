ee70bcd19e86fd5deff6126aa8484b6d09d1295d
Nimira43 <n.rabone72@aol.com>
2026-03-20 03:22:00 +0000

    Added validations to the CreatePostDto

--- src/posts/dtos/create-post.dto.ts
+++ src/posts/dtos/create-post.dto.ts
@@ -1,15 +1,51 @@
+import { IsArray, IsEnum, IsISO8601, IsJSON, IsNotEmpty, IsOptional, IsString, IsUrl, Matches, MinLength } from 'class-validator'
 import { postStatus } from '../enums/postStatus.enum'
 import { postType } from '../enums/postType.enum'
 
 export class CreatePostDto {
+  @IsString()
+  @MinLength(4)
+  @IsNotEmpty()
   title: string
+
+  @IsEnum(postType)
+  @IsNotEmpty()
   postType: postType
+  
+  @IsString()
+  @IsNotEmpty()
+  @Matches(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, {
+    message: 'A slug should be all small letters and can use "-". There should be no spaces. For example: "my-url"',
+
+  })
   slug: string
+  
+  @IsEnum(postStatus)
+  @IsNotEmpty()
   status: postStatus
-  content: string
-  schema: string
-  featuredImageUrl: string
-  publishedOn: Date
-  tags: string[]
-  metaOptions: [{key: 'some-key', value: 'some-value'}]
+  
+  @IsString()
+  @IsOptional()
+  content?: string
+
+  @IsOptional()
+  @IsJSON()
+  schema?: string
+
+  @IsOptional()
+  @IsUrl()
+  featuredImageUrl?: string
+
+  @IsISO8601()
+  @IsOptional()
+  publishedOn?: Date
+  
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  @MinLength(3, { each: true })
+  tags?: string[]
+
+  
+  metaOptions: [{key: 'some-key', value: true}]
 }
