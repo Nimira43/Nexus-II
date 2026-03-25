cb608dda3cfc20af77b8090c7815460e4a51199e
Nimira43 <n.rabone72@aol.com>
2026-03-25 18:38:19 +0000

    Nested DTO's

--- src/posts/dtos/create-post.dto.ts
+++ src/posts/dtos/create-post.dto.ts
@@ -1,6 +1,8 @@
-import { IsArray, IsEnum, IsISO8601, IsJSON, IsNotEmpty, IsOptional, IsString, IsUrl, Matches, MinLength } from 'class-validator'
+import { IsArray, IsEnum, IsISO8601, IsJSON, IsNotEmpty, IsOptional, IsString, IsUrl, Matches, MinLength, ValidateNested } from 'class-validator'
 import { postStatus } from '../enums/postStatus.enum'
 import { postType } from '../enums/postType.enum'
+import { CreatePostMetaOptionsDto } from './create-post-meta-options.dto'
+import { Type } from 'class-transformer'
 
 export class CreatePostDto {
   @IsString()
@@ -46,6 +48,9 @@ export class CreatePostDto {
   @MinLength(3, { each: true })
   tags?: string[]
 
-  
-  metaOptions: [{key: 'some-key', value: true}]
+  @IsOptional()
+  @IsArray()
+  @ValidateNested({ each: true})
+  @Type(() => CreatePostMetaOptionsDto)
+  metaOptions: CreatePostMetaOptionsDto[]
 }
