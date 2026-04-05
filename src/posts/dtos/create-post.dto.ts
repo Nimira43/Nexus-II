a7eab938b742662d2f5faef037b2648a4df2c806
Nimira43 <n.rabone72@aol.com>
2026-04-05 20:21:36 +0100

    Continuing Swagger documentation.

--- src/posts/dtos/create-post.dto.ts
+++ src/posts/dtos/create-post.dto.ts
@@ -3,17 +3,30 @@ import { postStatus } from '../enums/postStatus.enum'
 import { postType } from '../enums/postType.enum'
 import { CreatePostMetaOptionsDto } from './create-post-meta-options.dto'
 import { Type } from 'class-transformer'
+import { ApiProperty } from '@nestjs/swagger'
 
 export class CreatePostDto {
+  @ApiProperty({
+    example: 'This is the Title of this Post',
+    description: 'This is the title of the blog post.',
+  })
   @IsString()
   @MinLength(4)
   @IsNotEmpty()
   title: string
 
+  @ApiProperty({
+    enum: postType,
+    description: "Possible values: 'post', 'page', 'story', 'series'."
+  })
   @IsEnum(postType)
   @IsNotEmpty()
   postType: postType
   
+  @ApiProperty({
+    example: 'my-blog-post',
+    description: "For example: 'my-url'.",
+  })
   @IsString()
   @IsNotEmpty()
   @Matches(/^[a-z0-9]+(?:-[a-z0-9]+)*$/, {
@@ -22,6 +35,10 @@ export class CreatePostDto {
   })
   slug: string
   
+  @ApiProperty({
+    enum: postStatus,
+    description: "Possible value: 'draft', 'scheduled', 'review', 'published'.",
+  })
   @IsEnum(postStatus)
   @IsNotEmpty()
   status: postStatus
